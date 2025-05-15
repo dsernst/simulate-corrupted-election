305831d2d5a74c0e998ede43fb08f1caa84f559c
David Ernst <git@dsernst.com>
2025-05-15 07:41:10 -0700

    DRY test: intersection groups > should derive filter function from key correctly

--- app/utils/create-intersections.test.ts
+++ app/utils/create-intersections.test.ts
@@ -4,7 +4,7 @@ import {
   getIndentFromKey,
   getFilterFromKey,
 } from './create-intersections'
-import { TestType } from './calculateIntersections'
+import { TestType, VoteResult } from './calculateIntersections'
 
 type Key = string
 type Indent = number
@@ -43,30 +43,42 @@ describe('intersection groups', () => {
   })
 
   test('should derive filter function from key correctly', () => {
-    const mockVote = {
-      testedA: true,
-      testedB: true,
-      testedC: true,
-      testA: false,
-      testB: false,
-      testC: false,
+    type WasTested = [number, number, number]
+    type ShouldPass = boolean
+    const cases: [Key, WasTested, ShouldPass][] = [
+      // Simple cases
+      ['A', [1, 0, 1], true],
+      ['AB', [1, 1, 1], true],
+      ['ABC', [1, 1, 1], true],
+      ['ABC', [1, 1, 0], false],
+
+      // Exclusion cases
+      ['B!A', [0, 1, 1], true],
+      ['B!A', [1, 1, 1], false],
+      ['BC!A', [0, 1, 1], true],
+      ['BC!A', [1, 1, 1], false],
+      ['C!A!B', [0, 0, 1], true],
+      ['C!A!B', [1, 1, 1], false],
+    ]
+
+    /** Convert compressed [1, 0, 1] -> { testedA: true, testedB: false, testedC: true } */
+    function mockVoteFromWasTested(wasTested: WasTested): VoteResult {
+      return {
+        testedA: !!wasTested[0],
+        testedB: !!wasTested[1],
+        testedC: !!wasTested[2],
+        testA: undefined,
+        testB: undefined,
+        testC: undefined,
+      }
     }
 
-    // Test simple cases
-    expect(getFilterFromKey('A')(mockVote)).toBe(true)
-    expect(getFilterFromKey('AB')(mockVote)).toBe(true)
-    expect(getFilterFromKey('ABC')(mockVote)).toBe(true)
-
-    // Test exclusion cases
-    expect(getFilterFromKey('B!A')({ ...mockVote, testedA: false })).toBe(true)
-    expect(getFilterFromKey('B!A')({ ...mockVote, testedA: true })).toBe(false)
-    expect(getFilterFromKey('BC!A')({ ...mockVote, testedA: false })).toBe(true)
-    expect(getFilterFromKey('BC!A')({ ...mockVote, testedA: true })).toBe(false)
-    expect(
-      getFilterFromKey('C!A!B')({ ...mockVote, testedA: false, testedB: false })
-    ).toBe(true)
-    expect(getFilterFromKey('C!A!B')({ ...mockVote, testedA: true })).toBe(
-      false
-    )
+    // Run all our test cases
+    for (const [key, wasTested, shouldPass] of cases) {
+      const label = [key, wasTested, shouldPass].join()
+      const filterFunc = getFilterFromKey(key)
+      const mockVote = mockVoteFromWasTested(wasTested)
+      expect(filterFunc(mockVote), label).toBe(shouldPass)
+    }
   })
 })
