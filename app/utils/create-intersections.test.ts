f285fa5b4145b6c0dc35408fe89058c0d6287482
David Ernst <git@dsernst.com>
2025-05-15 07:11:35 -0700

    DRY create-intersections.test.ts

--- app/utils/create-intersections.test.ts
+++ app/utils/create-intersections.test.ts
@@ -1,42 +1,40 @@
 import { describe, expect, test } from 'bun:test'
 import { getTestsFromKey, getIndentFromKey } from './create-intersections'
+import { TestType } from './calculateIntersections'
+
+type Key = string
+type Indent = number
+type Tests = TestType[]
 
 describe('intersection groups', () => {
+  const testCases: [Key, Indent, Tests][] = [
+    // Individual tests
+    ['A', 0, ['A']],
+    ['B', 0, ['B']],
+    ['C', 0, ['C']],
+
+    // Two test combinations
+    ['AB', 1, ['A', 'B']],
+    ['B!A', 1, ['B']],
+    ['BC', 1, ['B', 'C']],
+    ['C!B', 1, ['C']],
+
+    // Three test combinations
+    ['ABC', 2, ['A', 'B', 'C']],
+    ['BC!A', 2, ['B', 'C']],
+    ['AC!B', 2, ['A', 'C']],
+    ['C!A!B', 2, ['C']],
+  ]
+
   test('should derive tests from key correctly', () => {
-    // Test individual tests
-    expect(getTestsFromKey('A')).toEqual(['A'])
-    expect(getTestsFromKey('B')).toEqual(['B'])
-    expect(getTestsFromKey('C')).toEqual(['C'])
-
-    // Test two test combinations
-    expect(getTestsFromKey('AB')).toEqual(['A', 'B'])
-    expect(getTestsFromKey('B!A')).toEqual(['B'])
-    expect(getTestsFromKey('BC')).toEqual(['B', 'C'])
-    expect(getTestsFromKey('C!B')).toEqual(['C'])
-
-    // Test three test combinations
-    expect(getTestsFromKey('ABC')).toEqual(['A', 'B', 'C'])
-    expect(getTestsFromKey('BC!A')).toEqual(['B', 'C'])
-    expect(getTestsFromKey('AC!B')).toEqual(['A', 'C'])
-    expect(getTestsFromKey('C!A!B')).toEqual(['C'])
+    testCases.forEach(([key, , tests]) => {
+      expect(getTestsFromKey(key)).toEqual(tests)
+    })
   })
 
   test('should derive indent level from key correctly', () => {
-    // Individual tests should have level 0
-    expect(getIndentFromKey('A')).toBe(0)
-    expect(getIndentFromKey('B')).toBe(0)
-    expect(getIndentFromKey('C')).toBe(0)
-
-    // Two test combinations should have level 1
-    expect(getIndentFromKey('AB')).toBe(1)
-    expect(getIndentFromKey('B!A')).toBe(1)
-    expect(getIndentFromKey('BC')).toBe(1)
-    expect(getIndentFromKey('C!B')).toBe(1)
-
-    // Three test combinations should have level 2
-    expect(getIndentFromKey('ABC')).toBe(2)
-    expect(getIndentFromKey('BC!A')).toBe(2)
-    expect(getIndentFromKey('AC!B')).toBe(2)
-    expect(getIndentFromKey('C!A!B')).toBe(2)
+    testCases.forEach(([key, indent]) => {
+      expect(getIndentFromKey(key)).toBe(indent)
+    })
   })
 })
