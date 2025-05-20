b47093daf538c170807dd6473c207eaca8aa99ce
David Ernst <git@dsernst.com>
2025-05-20 14:15:24 -0700

    Bit more px around app subtitle

--- app/page.tsx
+++ app/page.tsx
@@ -62,7 +62,7 @@ export default function Home() {
         <h1 className="text-4xl font-bold mb-2">
           Simulate Corrupted Elections
         </h1>
-        <p className="text-xl text-gray-600">
+        <p className="text-xl text-gray-600 px-4">
           How efficiently can you detect the compromised votes?
         </p>
       </div>
