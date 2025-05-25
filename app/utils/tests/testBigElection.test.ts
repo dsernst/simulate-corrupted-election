609aa182c06ea8159a1289a106153b64b1dceec8
David Ernst <git@dsernst.com>
2025-05-25 13:15:40 -0700

    unit test: Big election with lots of tests

--- app/utils/tests/testBigElection.test.ts
+++ app/utils/tests/testBigElection.test.ts
@@ -28,3 +28,26 @@ it('it can simulate big elections quickly', () => {
   expect(firstSetDuration).toBeLessThan(500)
   // console.log({ firstSetDuration })
 })
+
+it('it can simulate big elections with lots of tests quickly', () => {
+  const initStart = new Date()
+  // Initialize election
+  const s = new Simulator(BIG_SEED)
+  const { totalVotes } = s.election
+  const initDuration = msSince(initStart)
+
+  expect(totalVotes).toBe(2_028_787)
+  // Initializes in less than 5ms
+  expect(initDuration).toBeLessThan(10)
+
+  // Simulate: a50k b1k c50
+  const testSetStart = new Date()
+  s.test('a50000b1000c50')
+  const testSetDuration = msSince(testSetStart)
+  expect(s.get('A').compromises[0]).toBe(24701) // Confirm it ran, checking against known compromises
+
+  // Web GUI does it in ~12700ms
+  expect(testSetDuration).toBeGreaterThan(10) // getting ~2400ms
+  expect(testSetDuration).toBeLessThan(2500)
+  console.log({ testSetDuration })
+})
