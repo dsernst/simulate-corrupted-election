0fd4b58384c2cbe8eec82ced89fd6afd82ba44dc
David Ernst <git@dsernst.com>
2025-05-31 13:31:43 -0500

    Footer: date.tolocalString()

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,9 @@ export const Footer = () => {
       <i>
         {process.env.NEXT_PUBLIC_BUILD_TIMESTAMP ? (
           <>
-            Built at {process.env.NEXT_PUBLIC_BUILD_TIMESTAMP} · Latest Commit:{' '}
+            Built at{' '}
+            {new Date(process.env.NEXT_PUBLIC_BUILD_TIMESTAMP).toLocaleString()}{' '}
+            · Latest Commit:{' '}
             {process.env.NEXT_PUBLIC_GIT_COMMIT_SHA?.slice(0, 7)} · Branch:{' '}
             {process.env.NEXT_PUBLIC_GIT_BRANCH}
           </>
