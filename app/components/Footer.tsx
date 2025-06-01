bf4dae107e01a05f7f710b6c816b60893269963b
David Ernst <git@dsernst.com>
2025-05-31 23:57:48 -0500

    Tweak footer spacing

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <footer className="opacity-30 text-xs">
       Version:{' '}
-      <i className="inline-flex gap-1">
+      <i className="inline-flex gap-3">
         {process.env.NEXT_PUBLIC_BUILD_TIMESTAMP ? (
           <>
             <span>
@@ -12,10 +12,10 @@ export const Footer = () => {
               ).toLocaleString()}
             </span>
             <span>
-              Latest Commit: $
+              Latest Commit:{' '}
               {process.env.NEXT_PUBLIC_GIT_COMMIT_SHA?.slice(0, 7)}
             </span>
-            <span>Branch: ${process.env.NEXT_PUBLIC_GIT_BRANCH}</span>
+            <span>Branch: {process.env.NEXT_PUBLIC_GIT_BRANCH}</span>
           </>
         ) : (
           'localdev'
