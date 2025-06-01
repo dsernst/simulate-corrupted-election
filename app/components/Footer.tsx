08bf78b5084a9af83cb8cecff05300afec226e83
David Ernst <git@dsernst.com>
2025-06-01 00:07:14 -0500

    Improve look of <Footer />

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,26 +1,32 @@
 export const Footer = () => {
+  const buildTimestamp = process.env.NEXT_PUBLIC_BUILD_TIMESTAMP
+  const branch = process.env.NEXT_PUBLIC_GIT_BRANCH
+  const gitHash = process.env.NEXT_PUBLIC_GIT_COMMIT_SHA
+
   return (
-    <footer className="opacity-30 text-xs">
-      Version:{' '}
-      <i className="inline-flex gap-3">
-        {process.env.NEXT_PUBLIC_BUILD_TIMESTAMP ? (
-          <>
-            <span>
-              Built at{' '}
-              {new Date(
-                process.env.NEXT_PUBLIC_BUILD_TIMESTAMP
-              ).toLocaleString()}
-            </span>
-            <span>
-              Latest Commit:{' '}
-              {process.env.NEXT_PUBLIC_GIT_COMMIT_SHA?.slice(0, 7)}
-            </span>
-            <span>Branch: {process.env.NEXT_PUBLIC_GIT_BRANCH}</span>
-          </>
-        ) : (
-          'localdev'
-        )}
-      </i>
+    <footer className="opacity-30 text-xs italic inline-flex gap-3">
+      {buildTimestamp && (
+        <>
+          <span className="not-italic">Version:</span>
+          <span>Deployed {new Date(buildTimestamp).toLocaleString()}</span>
+          <span>·</span>
+          <span>
+            Latest Commit:{' '}
+            <a
+              className="hover:underline cursor-pointer"
+              href={`https://github.com/dsernst/simulate-corrupted-election/`}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              {gitHash?.slice(0, 7)}
+            </a>
+          </span>
+          <span>·</span>
+          {branch && !['main', 'unknown'].includes(branch) && (
+            <span>Branch: {branch}</span>
+          )}
+        </>
+      )}
     </footer>
   )
 }
