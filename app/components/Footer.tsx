7342470513dc6014ba9886d9ae6c374b4c8c44b1
David Ernst <git@dsernst.com>
2025-06-01 00:46:10 -0500

    Simplify footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,26 +1,27 @@
+import TimeAgo from 'timeago-react'
+
+import { version } from '../../package.json'
+
 export const Footer = () => {
   const buildTimestamp = process.env.NEXT_PUBLIC_BUILD_TIMESTAMP
   const branch = process.env.NEXT_PUBLIC_GIT_BRANCH
   const gitHash = process.env.NEXT_PUBLIC_GIT_COMMIT_SHA
 
   return (
-    <footer className="opacity-30 text-xs italic inline-flex gap-3">
+    <footer className="opacity-30 text-xs inline-flex gap-3">
+      <span>v{version}</span>
       {buildTimestamp && (
         <>
-          <span className="not-italic">Version:</span>
-          <span>Deployed {new Date(buildTimestamp).toLocaleString()}</span>
           <span>·</span>
-          <span>
-            Latest Commit:{' '}
-            <a
-              className="hover:underline cursor-pointer"
-              href={`https://github.com/dsernst/simulate-corrupted-election/`}
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              {gitHash?.slice(0, 7)}
-            </a>
-          </span>
+          <a
+            className="hover:underline cursor-pointer"
+            href={`https://github.com/dsernst/simulate-corrupted-election/`}
+            rel="noopener noreferrer"
+            target="_blank"
+            title={`Git commit: ${gitHash?.slice(0, 7)}`}
+          >
+            Built <TimeAgo datetime={buildTimestamp} />
+          </a>
           {branch && !['main', 'unknown'].includes(branch) && (
             <>
               <span>·</span>
